5cd2fcc31af2e105483fd87f4f145ac870b9bd8a
MAAZ ZAMA <49765187+maaaz15@users.noreply.github.com>
2023-10-27 17:18:11 +0530

    Fixed typos

--- script.js
+++ script.js
@@ -6,23 +6,23 @@ form.addEventListener('submit', (e) => {
     const temp = document.querySelector("#temp").value
     const units = document.querySelector("#units").value
 
-    // Celcius
+    // Celsius
 
-    const celToFah = (celcius) => {
-        let fahrenheit = ((celcius * 9 / 5) + 32)
+    const celToFah = (celsius) => {
+        let fahrenheit = ((celsius * 9 / 5) + 32)
         return fahrenheit
     }
 
-    const celToKel = (celcius) => {
-        let kelvin = (celcius + 273.15)
+    const celToKel = (celsius) => {
+        let kelvin = (celsius + 273.15)
         return kelvin
     }
 
     // Fahrenheit
 
     const fahToCel = (fahrenheit) => {
-        let celcius = ((fahrenheit - 32) / 1.8)
-        return celcius
+        let celsius = ((fahrenheit - 32) / 1.8)
+        return celsius
     }
 
     const fahToKel = (fahrenheit) => {
@@ -33,8 +33,8 @@ form.addEventListener('submit', (e) => {
     // Kelvin
 
     const kelToCel = (kelvin) => {
-        let celcius = (kelvin - 273.15)
-        return celcius
+        let celsius = (kelvin - 273.15)
+        return celsius
     }
 
     const kelToFah = (kelvin) => {
@@ -46,7 +46,7 @@ form.addEventListener('submit', (e) => {
         result.innerHTML = 'Please enter valid input'
     }
     else{
-        if (units === 'celcius') {
+        if (units === 'celsius') {
             result = celToFah(temp)
             resultalt = celToKel(temp)
             document.querySelector('#result').innerHTML = `${result.toFixed(2)}° Fahrenheit`;
@@ -65,7 +65,7 @@ form.addEventListener('submit', (e) => {
         else{
             result = kelToCel(temp)
             resultalt = kelToFah(temp)
-            document.querySelector('#result').innerHTML = `${result.toFixed(2)}° Celcius`;
+            document.querySelector('#result').innerHTML = `${result.toFixed(2)}° Celsius`;
             document.querySelector('#resultalt').innerHTML = `${resultalt.toFixed(2)}° Fahrenheit`;
         }
     }
