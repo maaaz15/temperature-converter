8980dbd25a3d3f376f1675b7de444d4a48a1bf8c
MAAZ ZAMA <49765187+maaaz15@users.noreply.github.com>
2023-10-27 17:13:01 +0530

    Conversion to Kelvin

--- script.js
+++ script.js
@@ -3,17 +3,43 @@ const form = document.querySelector('form')
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const temp = parseInt(document.querySelector("#temp").value)
+    const temp = document.querySelector("#temp").value
     const units = document.querySelector("#units").value
+
+    // Celcius
 
     const celToFah = (celcius) => {
         let fahrenheit = ((celcius * 9 / 5) + 32)
         return fahrenheit
     }
 
+    const celToKel = (celcius) => {
+        let kelvin = (celcius + 273.15)
+        return kelvin
+    }
+
+    // Fahrenheit
+
     const fahToCel = (fahrenheit) => {
-        let celcius = ((fahrenheit - 32) * 5 / 9)
+        let celcius = ((fahrenheit - 32) / 1.8)
         return celcius
+    }
+
+    const fahToKel = (fahrenheit) => {
+        let kelvin = ((fahrenheit - 32) / 1.8 + 273.15)
+        return kelvin
+    }
+
+    // Kelvin
+
+    const kelToCel = (kelvin) => {
+        let celcius = (kelvin - 273.15)
+        return celcius
+    }
+
+    const kelToFah = (kelvin) => {
+        let fahrenheit = ((kelvin - 273.15) * 9 / 5 + 32)
+        return fahrenheit
     }
 
     if (temp === "" || isNaN(temp)) {
@@ -22,13 +48,25 @@ form.addEventListener('submit', (e) => {
     else{
         if (units === 'celcius') {
             result = celToFah(temp)
+            resultalt = celToKel(temp)
             document.querySelector('#result').innerHTML = `${result.toFixed(2)}° Fahrenheit`;
+            document.querySelector('#resultalt').innerHTML = `${resultalt.toFixed(2)}° Kelvin`;
             console.log(result);
+            console.log(resultalt);
+        }
+        else if(units === 'fahrenheit'){
+            result = fahToCel(temp)
+            resultalt = fahToKel(temp)
+            document.querySelector('#result').innerHTML = `${result.toFixed(2)}° Celsius`;
+            document.querySelector('#resultalt').innerHTML = `${resultalt.toFixed(2)}° Kelvin`;
+            console.log(result);
+            console.log(resultalt);
         }
         else{
-            result = fahToCel(temp)
-            document.querySelector('#result').innerHTML = `${result.toFixed(2)}° Celsius`;
-            console.log(result);
+            result = kelToCel(temp)
+            resultalt = kelToFah(temp)
+            document.querySelector('#result').innerHTML = `${result.toFixed(2)}° Celcius`;
+            document.querySelector('#resultalt').innerHTML = `${resultalt.toFixed(2)}° Fahrenheit`;
         }
     }
 
